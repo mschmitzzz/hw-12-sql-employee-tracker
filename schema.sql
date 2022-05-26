dd19305f0398d107c620cf957dc5134b5098e773
Mac Schmitz <mac.schmitz@att.net>
2022-05-26 14:44:39 -0400

    added primary and foreign keys

--- schema.sql
+++ schema.sql
@@ -4,21 +4,33 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE roles(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employees(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 );
